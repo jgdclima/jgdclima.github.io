cf29eed9cf6d526ed0def0b1212277fe50534320
Brazil <theotherlima@gmail.com>
2019-04-10 19:05:29 -0700

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Header.js';
+import './Banner.js';
+import './Button.js';
 
 class App extends Component {
   render() {
