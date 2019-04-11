bc3e272a7f4ad2dddc9b3a7679c7dee37aa3f454
Brazil <theotherlima@gmail.com>
2019-04-11 11:05:34 -0700

    Added Tabs.js, Tab.js, and reformatted code to fit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import './App.css';
 import Header from './Header';
 import Banner from './Banner';
 import Button from './Button';
+import Tabs from './Tabs'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <Banner></Banner>
+        <Tabs>
+          <div label="JL">
+            Home
+            <Banner></Banner>
+          </div>
+          <div label="Projects">
+            Projects
+          </div>
+          <div label="Creative">
+            Creative
+          </div>
+          <div label="Case Studies">
+            Case Studies
+          </div>
+          <div label="Resume">
+            Resume
+          </div>
+        </Tabs>
       </div>
     );
   }
---
+++ src/Tab.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+function Tab(props){
+
+  static PropTypes = {
+    activeTab: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
+
+  onClick = () => {
+    const { label, onClick } = this.props;
+    onClick(label);
+  }
+
+  render() {
+    const {
+      onClick,
+      props: {
+        activeTab,
+        label,
+      },
+    } = this;
+
+    let className = 'tab-list-item';
+
+    if (activeTab === label) {
+      className += ' tab-list-active';
+    }
+
+  return(
+    <div className="Tab">
+      {props.children}
+    </div>
+  );
+}
