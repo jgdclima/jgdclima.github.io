3098a50b1bea65a572ba9f1db1763120029e1358
Brazil <theotherlima@gmail.com>
2019-04-11 01:34:38 -0700

    added Sass

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,11 @@ import Button from './Button';
 function Header(props){
   return(
     <div className="Header">
-      <Button text="JL" fun=""></Button>
-      <Button text="Projects" fun=""></Button>
-      <Button text="Creative" fun=""></Button>
-      <Button text="Case Studies" fun=""></Button>
-      <Button text="Resume" fun=""></Button>
+      <Button class='primary' text="JL" fun=""></Button>
+      <Button class='secondary' text="Projects" fun=""></Button>
+      <Button class='secondary' text="Creative" fun=""></Button>
+      <Button class='secondary' text="Case Studies" fun=""></Button>
+      <Button class='secondary' text="Resume" fun=""></Button>
     </div>
   );
 }
