98eeab5e1a4cb6665e705b8f5b0754fa2d9b8324
Brazil <theotherlima@gmail.com>
2019-04-10 19:20:29 -0700

    trying to fix imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Header from './Header.js';
+import * as Banner from './Banner.js';
+import * as Button from './Button.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
